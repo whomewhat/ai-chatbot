d88e7a8c7acf4d9ca65a809bddbc51890cf18372
whomewhat <134745293+whomewhat@users.noreply.github.com>
2024-01-07 19:27:07 -0600

    Update page.tsx
    updated to use my botcard

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -1,8 +1,18 @@
-import { nanoid } from '@/lib/utils'
-import { Chat } from '@/components/chat'
+import React from 'react';
+import { nanoid } from '@/lib/utils';
+import BotCard from '../components/BotCard';
+import bots from '../bots';
+import { Chat } from '@/components/chat';
 
 export default function IndexPage() {
-  const id = nanoid()
+  const id = nanoid();
 
-  return <Chat id={id} />
+  return (
+    <div>
+      <Chat id={id} />
+      <h1>My Custom GPT Creations</h1>
+      <h2>Take a look at our featured creations</h2>
+      {bots.map((bot, index) => <BotCard key={index} bot={bot} />)}
+    </div>
+  );
 }
